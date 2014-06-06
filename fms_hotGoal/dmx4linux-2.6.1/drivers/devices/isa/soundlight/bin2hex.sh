e1bfba6e4384e137d7b0963e9ca71e6b9b219f0c
timytamy <timytamy@gmail.com>
2014-06-06 10:31:00 +1000

    python version is working fine, so deleted all the other dead ends

--- fms_hotGoal/dmx4linux-2.6.1/drivers/devices/isa/soundlight/bin2hex.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-STATIC=static
-NAME=`basename $1 .asm`
-
-echo "$STATIC unsigned char ${NAME}_bin [] = {"
-od -tx1 -v | cut -b9-100 | sed -e 's/[0-9a-zA-Z][0-9a-zA-Z]/0x&,/g'
-echo "};"
-echo "$STATIC DMXBinary firmware_$NAME = {\"$NAME\", sizeof(${NAME}_bin), ${NAME}_bin};"
