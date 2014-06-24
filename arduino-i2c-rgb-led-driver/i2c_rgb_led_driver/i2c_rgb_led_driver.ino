2cec596953a22ad0b1f257091f12f65d6b0b3b6c
timytamy <timytamy@gmail.com>
2014-06-24 14:30:26 +1000

    finished LED thingy

--- arduino-i2c-rgb-led-driver/i2c_rgb_led_driver/i2c_rgb_led_driver.ino
+++ arduino-i2c-rgb-led-driver/i2c_rgb_led_driver/i2c_rgb_led_driver.ino
@@ -1,61 +1,82 @@
 /*
   i2c_rgb_led_driver
 
-  Drives a RGB LED strip with MOSFETS, taking comands
+  Drives a Rgb LED strip with MOSFETS, taking comands
   from a I2C master
   
   Created 20131224
   By Tim
   
-  https://github.com/timytamy/temp-projects
+  https://github.com/timytamy/projects
 
 */
 
 #include <Wire.h>
 
-#define PIN_RED 9
-#define PIN_GREEN 3
-#define PIN_BLUE 10
-#define I2C_ADDRESS (0b10110001)
 #define PIN_LED 13
+#define PIN_RED 5
+#define PIN_GREEN 6
+#define PIN_BLUE 3
+#define I2C_ADDRESS (0b10110001)
+
+#define LED_TEST false
 
 void setup() {
+  pinMode(PIN_LED, OUTPUT);
   pinMode(PIN_RED, OUTPUT);
   pinMode(PIN_GREEN, OUTPUT);
   pinMode(PIN_BLUE, OUTPUT);
+
   Wire.begin(I2C_ADDRESS);
   Wire.onReceive(handlerRecieve);
-  //Wire.onRequest(handlerRequest);
-  pinMode(PIN_LED, OUTPUT);
+  Wire.onRequest(handlerRequest);
+
+  // Start off with green
+  setColourRgb(0, 255, 0);
 }
 
 void loop() {
+  if (LED_TEST == true) {
+    uint8_t rgbMix[3];
+    for (int i = 0; i < 3; i++) rgbMix[i] = 0;
+    for (int i = 0; i < 3; i++) {
+      for (rgbMix[i] = 0; rgbMix[i] < 255; rgbMix[i]++) {
+        setColourRgb(rgbMix[0], rgbMix[1], rgbMix[2]);
+        delay(10);
+      }
+      rgbMix[i] = 0;
+    }
+  }
 }
 
 void setColour(int pin, uint8_t value) {
   analogWrite(pin, value);
 }
 
-void setColourRGB(uint8_t red, uint8_t green, uint8_t blue) {
+void setColourRgb(uint8_t red, uint8_t green, uint8_t blue) {
   setColour(PIN_RED, red);
   setColour(PIN_GREEN, green);
   setColour(PIN_BLUE, blue);
 }
 
-void handlerRecieve(int numBytes) {
-  digitalWrite(PIN_LED, HIGH); //D13 LED is flashed on when data is being recieved
-  //Parse input into bytesIn array
-  uint8_t bytesIn[numBytes];
-  for (int i = 0; i < numBytes; i++) {
+void handlerRecieve(int numBytesIn) {
+  // Turn on led on i2c recieve
+  digitalWrite(PIN_LED, HIGH);
+
+  // Parse input into bytesIn array
+  uint8_t bytesIn[numBytesIn];
+  for (int i = 0; i < numBytesIn; i++) {
     bytesIn[i] = Wire.read();
   }
-  //Set Colours
-  if (numBytes >= 3){
-    setColourRGB(bytesIn[0], bytesIn[1], bytesIn[2]);
+
+  // Set Colours
+  if (numBytesIn >= 3){
+    setColourRgb(bytesIn[0], bytesIn[1], bytesIn[2]);
   }
-  digitalWrite(PIN_LED, LOW); //Turning D13 LED off again
+  
+  digitalWrite(PIN_LED, LOW);
 }
 
-//void handlerRequest() {} //Not implemented and not needed
+void handlerRequest() {} //Not implemented and not needed
 
 
