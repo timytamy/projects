885f5ee4d6b5b4b6d95a5eec182f7b1ac0338dac
timytamy <timytamy@gmail.com>
2015-05-23 16:35:29 +1000

    some changes for susan

--- 2015_pennyCode/pennyCode.py
+++ 2015_pennyCode/pennyCode.py
@@ -5,12 +5,12 @@ import string
 import sys
 
 def main():
-    if len(sys.argv) <= 2: return
-    elif len(sys.argv) == 3:
+    if len(sys.argv) <= 2: printUsage(); return
+    elif len(sys.argv) == 3: #Cipher and text is given
         cipher, command, layers, strIn = sys.argv[1].lower(), "",  1, sys.argv[2]
-    elif len(sys.argv) == 4:
+    elif len(sys.argv) == 4: #Cipher, encode/decode and text is given
         cipher, command, layers, strIn = sys.argv[1].lower(), sys.argv[2].lower(), 1, sys.argv[3]
-    elif len(sys.argv) == 5:
+    elif len(sys.argv) == 5: #Cipher, encode/decode, numOfIterations and text is given
         cipher, command, layers, strIn = sys.argv[1].lower(), sys.argv[2].lower(), int(sys.argv[3]), sys.argv[4]
 
     if command in "encode encrypt": decrypt = False
@@ -22,7 +22,7 @@ def main():
         if (cipher in "pennycode"): strOut = pennyCode(strOut, decrypt)
         elif (cipher in "rot13"): strOut = rot13(strOut)
         elif (cipher in "rot47"): strOut = rot47(strOut)
-        elif (cipher in "rotx"): strOut = rotX(strOut, decrypt, 1)
+        elif (cipher in "rotx"): strOut = rotX(strOut, decrypt, layers); break
         elif (cipher in "blakeapproves"): strOut = blakeApproves(strOut, decrypt)
         elif (cipher in "lazysod"): strOut = lazySod(strOut, decrypt)
         else: print "ERROR: unrecognised cipher"; return
@@ -61,14 +61,8 @@ def rot13 (strIn):
 
 
 def rot47 (strIn):
-    charsOut = []
-    for i in range(len(strIn)):
-        if (33 <= ord(strIn[i]) and ord(strIn[i]) <= 126):
-            charsOut.append(chr(33 + (ord(strIn[i]) + 14) % 94)) #47-33 == 14
-        else:
-            charsOut.append(strIn[i])
+    return rotX(strIn, True, 47)
 
-    return ''.join(charsOut)
 
 def rotX (strIn, decrypt, rotValue):
     charsOut = []
@@ -100,13 +94,19 @@ def blakeApproves (strIn, decrypt = False, key = ["Blake", "Approves", ".com"]):
             charsOut.append(chr(charCurr)) #List of chars
         strOut = ''.join(charsOut) #Join string up
     else:
-        strOut = ((key[2]+" ").join((" ".join(key[0] if bit == "0" else key[1] for bit in bin(ord(char))[2:])) for char in strIn)) #wtf
+        strOut = (key[2].join((" ".join(key[0] if bit == "0" else key[1] for bit in bin(ord(char))[2:])) for char in strIn)) #wtf
 
     return strOut
 
 
 def lazySod (strIn, decrypt = False):
     return blakeApproves(strIn, decrypt, ["Lazy", "Sod", ".com "])
+
+
+def printUsage ():
+    print "USAGE:\t python " + sys.argv[0] + " [CODE] [TEXT]"
+    print "\t python " + sys.argv[0] + " [CODE] [ENCRYPT/DECRYPT] [TEXT]"
+    print "\t python " + sys.argv[0] + " [CODE] [ENCRYPT/DECRYPT] [NUM ITERATIONS] [TEXT]"
 
 
 if (__name__ == "__main__"): main()
